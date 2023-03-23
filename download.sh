c5b1a966ccd8d862ce881bc5a0608943b4b5c3d0
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-03-23 20:36:28 +0600

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,8 @@
 pwd
 cd /usr/src/app
-gdown https://drive.google.com/uc?id=17W1-cSkJzERQ7lOGAQaCVNOPUIlw8c9B
-mkdir -pv ncs && unzip -d ./ncs/ -j ncs.zip
-rm -rf ncs.zip
-ls -a ncs
+#gdown https://drive.google.com/uc?id=17W1-cSkJzERQ7lOGAQaCVNOPUIlw8c9B
+#mkdir -pv ncs && unzip -d ./ncs/ -j ncs.zip
+#rm -rf ncs.zip
+#ls -a ncs
+yt-dlp -f 'bv*[height=1080]+ba' --download-archive videos.txt  https://youtube.com/playlist?list=PLRBp0Fe2GpgmsW46rJyudVFlY6IYjFBIK -o '%(channel_id)s/%(playlist_id)s/%(id)s.%(ext)s'
+ls -a
