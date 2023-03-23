0348592c0fed2a0bfa8a834836f722a61769ff3b
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-03-23 19:31:31 +0600

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-ls -a && pwd
-ls -a /usr/src/app
+#!/bin/bash
+
+while true; do
+  ffmpeg -loglevel info -y -re \
+         -f concat -safe 0 -i <((for f in ./ncs/*.mkv; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
+         -c:a aac -ar 44100 -b:a 128k \
+         -c:v libx264 -pix_fmt yuv420p -preset veryfast -g 60 \
+         -b:v 2500k -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
+done
