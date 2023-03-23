db2c13768e0d0ee094d5d636ef69bbc1d2a88c54
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-03-23 15:42:09 +0600

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-set -e
+VIDEO_DIR=./"Ben 10 Alien Force 2008 Season 1 Complete Web 720p x264 [i_c]"/
 
-while true
-do
-  ffmpeg -loglevel info -y -re \
-    -f concat -safe 0 -i <((for f in ./"Ben 10 Alien Force 2008 Season 1 Complete Web 720p x264 [i_c]"/*.mkv; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
-    -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
-    -framerate 30 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
-    -c:a aac -b:a 128k -ar 44100 \
-    -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
+while true; do
+  VIDEO_FILE=$(ls $VIDEO_DIR | shuf -n 1)
+  ffmpeg -loglevel info -stream_loop -1 \
+         -y -re -i $VIDEO_DIR/$VIDEO_FILE \
+         -c:a aac -ar 44100 -b:a 128k \
+         -c:v libx264 -pix_fmt yuv420p -preset veryfast -g 60 \
+         -b:v 2500k -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
 done
