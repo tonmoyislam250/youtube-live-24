76c29c45abf348800593e3b1228dd91b7a0fc82b
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-03-23 16:56:34 +0600

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -2,7 +2,7 @@
 
 while true; do
   ffmpeg -loglevel info -y -re \
-         -f concat -safe 0 -i <((for f in ./mp3/*.mp3; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
+         -f concat -safe 0 -i <((for f in ./Ben10S01/*.mkv; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
          -c:a aac -ar 44100 -b:a 128k \
          -c:v libx264 -pix_fmt yuv420p -preset veryfast -g 60 \
          -b:v 2500k -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
