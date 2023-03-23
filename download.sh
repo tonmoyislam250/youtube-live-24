a93c3b51e33d3ae4b042ee0a75f58c07e2984458
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-03-23 19:35:47 +0600

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,7 +1,6 @@
 pwd
 cd /usr/src/app
 gdown https://drive.google.com/uc?id=17W1-cSkJzERQ7lOGAQaCVNOPUIlw8c9B
-unzip ncs.zip
-mv "NCS ： Top 100 📈.zip" ncs
+mkdir -pv ncs && unzip ncs.zip -d ./ncs/
 rm -rf ncs.zip
 ls -a ncs
