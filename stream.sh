ba7633a6b2cdde40e2c2c01143f2de27d612818f
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-03-23 14:43:46 +0600

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -5,7 +5,7 @@ set -e
 while true
 do
   ffmpeg -loglevel info -y -re \
-    -f image2 -loop 1 -i bg.png \
+#    -f image2 -loop 1 -i bg.png \
     -f concat -safe 0 -i <((for f in ./"Ben 10 Alien Force 2008 Season 1 Complete Web 720p x264 [i_c]"/*.mkv; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
     -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
     -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
