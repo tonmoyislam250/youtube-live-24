fb08c0ba1292099d2bbc415853f3478865e23d96
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-03-23 16:46:25 +0600

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 while true; do
-  VIDEO_FILE=$(ls ./Ben10S01/ | shuf -n 1)
-  ffmpeg -loglevel info -stream_loop -1 \
-         -y -re -i ./Ben10S01/$VIDEO_FILE \
+  ffmpeg -loglevel info -y -re \
+         -f concat -safe 0 -i <((for f in ./mp3/*.mp3; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
          -c:a aac -ar 44100 -b:a 128k \
          -c:v libx264 -pix_fmt yuv420p -preset veryfast -g 60 \
          -b:v 2500k -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
