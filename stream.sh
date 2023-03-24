df6a6f482e581d995b2c1d3d1c21ea46fe20cae1
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-03-24 12:26:48 +0600

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -8,7 +8,7 @@ ls -a ncs
 
 while true; do
   ffmpeg -loglevel info -y -re \
-         -f concat -safe 0 -i <((for f in ./youtube/*.mkv; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
+         -f concat -safe 0 -i <((for f in ./ncs/*.mkv; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
          -c:a aac -ar 44100 -b:a 128k \
          -c:v libx264 -pix_fmt yuv420p -preset veryfast -g 60 \
          -b:v 2500k -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
