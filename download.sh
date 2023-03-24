17b4dc83bcbb01638fdd9b5b1d9ffa4144075a16
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-03-24 11:24:17 +0600

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 pwd
 cd /usr/src/app
-gdown -V https://drive.google.com/uc?id=1crRYTqqJkYrudw18dQtxtugnQ10wTRBz
+gdown --speed https://drive.google.com/uc?id=1crRYTqqJkYrudw18dQtxtugnQ10wTRBz
 mkdir -pv ncs && unzip -d ./ncs/ -j ncs2.zip
 rm -rf ncs.zip
 ls -a ncs
