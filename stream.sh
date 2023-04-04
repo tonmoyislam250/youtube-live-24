36a1eb6050f3c18cbb5ab42825f922f969a7a5cf
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-04-04 17:28:58 +0600

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -8,7 +8,7 @@ rm -rf *.zip
 
 while true; do
   ffmpeg -loglevel info -y -re \
-         -f concat -safe 0 -i <((for f in ./temp/*.mkv; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
+         -f concat -safe 0 -i <((for f in temp/*.mkv; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
          -c:a aac -ar 44100 -b:a 128k \
          -c:v libx264 -pix_fmt yuv420p -preset veryfast -g 60 \
          -b:v 2500k -f flv $STREAM_URL/$STREAM_KEY
