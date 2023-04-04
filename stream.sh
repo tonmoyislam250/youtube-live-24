862ef914966b7e67dec393ddab04af20ce023894
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-04-04 18:00:32 +0600

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -11,5 +11,5 @@ while true; do
          -f concat -safe 0 -i <((for f in temp/*.avi; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
          -c:a aac -ar 44100 -b:a 128k \
          -c:v libx264 -pix_fmt yuv420p -preset veryfast -g 60 \
-         -b:v 2500k -f flv $STREAM_URL/$STREAM_KEY
+         -b:v 2500k -f flv $STREAM_URL/app/$STREAM_KEY
 done
