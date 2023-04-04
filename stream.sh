f55ed4775355192821db67c46997b62abcd23f30
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-04-04 16:59:39 +0600

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -3,13 +3,12 @@ pwd
 cd /usr/src/app
 rm -rf *
 gdown $LINK
-mkdir -pv ncs && unzip -d ./ncs/ -j *.zip
+#mkdir -pv ncs && unzip -d ./ncs/ -j *.zip
 rm -rf *.zip
-ls -a ncs
 
 while true; do
   ffmpeg -loglevel info -y -re \
-         -f concat -safe 0 -i <((for f in ./ncs/*.mkv; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
+         -f concat -safe 0 -i <((for f in ./$FOLDER_NAME/*.mkv; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
          -c:a aac -ar 44100 -b:a 128k \
          -c:v libx264 -pix_fmt yuv420p -preset veryfast -g 60 \
          -b:v 2500k -f flv $STREAM_URL/$STREAM_KEY
